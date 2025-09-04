bc27d5307a966958d879c1c75b6f6161a4dbbedd
felix <felix.lwestin@gmail.com>
2025-09-04 21:47:26 +0200

    Initial Commit

---
+++ GigiCopyHelper/GigiCopyHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  GigiCopyHelperApp.swift
+//  GigiCopyHelper
+//
+//  Created by Felix Westin on 2025-09-04.
+//
+
+import SwiftUI
+
+@main
+struct GigiCopyHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GigiCopyHelperTests/GigiCopyHelperTests.swift
@@ -0,0 +1,17 @@
+//
+//  GigiCopyHelperTests.swift
+//  GigiCopyHelperTests
+//
+//  Created by Felix Westin on 2025-09-04.
+//
+
+import Testing
+@testable import GigiCopyHelper
+
+struct GigiCopyHelperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
